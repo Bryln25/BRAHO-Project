f64d9c7a06f6607609d9f46da4f8114bc9ac8a1b
Innova Services SRL <Innova Services SRL@BRYLN>
2025-09-02 10:31:18 -0400

    Agregar archivos de proyecto.

---
+++ BRAHO Project.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace BRAHO_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            MenuVertical = new Panel();
+            BotonVentas = new Button();
+            BotonRegistrarObra = new Button();
+            BotonClientes = new Button();
+            pictureBox1 = new PictureBox();
+            BarraTitulo = new Panel();
+            BotonMininizar = new PictureBox();
+            BotonCerrar = new PictureBox();
+            BotonSlide = new PictureBox();
+            Contenedor = new Panel();
+            MenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            BarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)BotonMininizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BotonCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BotonSlide).BeginInit();
+            SuspendLayout();
+            // 
+            // MenuVertical
+            // 
+            MenuVertical.BackColor = Color.FromArgb(127, 127, 127);
+            MenuVertical.Controls.Add(BotonVentas);
+            MenuVertical.Controls.Add(BotonRegistrarObra);
+            MenuVertical.Controls.Add(BotonClientes);
+            MenuVertical.Controls.Add(pictureBox1);
+            MenuVertical.Dock = DockStyle.Left;
+            MenuVertical.Location = new Point(0, 0);
+            MenuVertical.Name = "MenuVertical";
+            MenuVertical.Size = new Size(250, 650);
+            MenuVertical.TabIndex = 0;
+            // 
+            // BotonVentas
+            // 
+            BotonVentas.FlatAppearance.BorderSize = 0;
+            BotonVentas.FlatAppearance.MouseOverBackColor = Color.FromArgb(102, 102, 102);
+            BotonVentas.FlatStyle = FlatStyle.Flat;
+            BotonVentas.Font = new Font("Century Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BotonVentas.Image = (Image)resources.GetObject("BotonVentas.Image");
+            BotonVentas.ImageAlign = ContentAlignment.MiddleLeft;
+            BotonVentas.Location = new Point(0, 286);
+            BotonVentas.Name = "BotonVentas";
+            BotonVentas.Size = new Size(250, 52);
+            BotonVentas.TabIndex = 4;
+            BotonVentas.Text = "Ventas";
+            BotonVentas.UseVisualStyleBackColor = true;
+            // 
+            // BotonRegistrarObra
+            // 
+            BotonRegistrarObra.FlatAppearance.BorderSize = 0;
+            BotonRegistrarObra.FlatAppearance.MouseOverBackColor = Color.FromArgb(102, 102, 102);
+            BotonRegistrarObra.FlatStyle = FlatStyle.Flat;
+            BotonRegistrarObra.Font = new Font("Century Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BotonRegistrarObra.Image = (Image)resources.GetObject("BotonRegistrarObra.Image");
+            BotonRegistrarObra.ImageAlign = ContentAlignment.MiddleLeft;
+            BotonRegistrarObra.Location = new Point(0, 228);
+            BotonRegistrarObra.Name = "BotonRegistrarObra";
+            BotonRegistrarObra.Size = new Size(250, 52);
+            BotonRegistrarObra.TabIndex = 2;
+            BotonRegistrarObra.Text = "Obras";
+            BotonRegistrarObra.UseVisualStyleBackColor = true;
+            // 
+            // BotonClientes
+            // 
+            BotonClientes.FlatAppearance.BorderSize = 0;
+            BotonClientes.FlatAppearance.MouseOverBackColor = Color.FromArgb(102, 102, 102);
+            BotonClientes.FlatStyle = FlatStyle.Flat;
+            BotonClientes.Font = new Font("Century Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BotonClientes.Image = (Image)resources.GetObject("BotonClientes.Image");
+            BotonClientes.ImageAlign = ContentAlignment.MiddleLeft;
+            BotonClientes.Location = new Point(0, 170);
+            BotonClientes.Name = "BotonClientes";
+            BotonClientes.Size = new Size(250, 52);
+            BotonClientes.TabIndex = 3;
+            BotonClientes.Text = "Clientes";
+            BotonClientes.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-24, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(251, 89);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // BarraTitulo
+            // 
+            BarraTitulo.BackColor = Color.FromArgb(102, 102, 102);
+            BarraTitulo.Controls.Add(BotonMininizar);
+            BarraTitulo.Controls.Add(BotonCerrar);
+            BarraTitulo.Controls.Add(BotonSlide);
+            BarraTitulo.Dock = DockStyle.Top;
+            BarraTitulo.Location = new Point(250, 0);
+            BarraTitulo.Name = "BarraTitulo";
+            BarraTitulo.Size = new Size(1050, 50);
+            BarraTitulo.TabIndex = 1;
+            BarraTitulo.MouseDown += BarraTitulo_MouseDown;
+            // 
+            // BotonMininizar
+            // 
+            BotonMininizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BotonMininizar.Cursor = Cursors.Hand;
+            BotonMininizar.Image = (Image)resources.GetObject("BotonMininizar.Image");
+            BotonMininizar.Location = new Point(968, 9);
+            BotonMininizar.Name = "BotonMininizar";
+            BotonMininizar.Size = new Size(29, 33);
+            BotonMininizar.SizeMode = PictureBoxSizeMode.Zoom;
+            BotonMininizar.TabIndex = 1;
+            BotonMininizar.TabStop = false;
+            BotonMininizar.Click += BotonMininizar_Click;
+            // 
+            // BotonCerrar
+            // 
+            BotonCerrar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BotonCerrar.Cursor = Cursors.Hand;
+            BotonCerrar.Image = (Image)resources.GetObject("BotonCerrar.Image");
+            BotonCerrar.Location = new Point(1009, 9);
+            BotonCerrar.Name = "BotonCerrar";
+            BotonCerrar.Size = new Size(29, 33);
+            BotonCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            BotonCerrar.TabIndex = 0;
+            BotonCerrar.TabStop = false;
+            BotonCerrar.Click += BotonCerrar_Click;
+            // 
+            // BotonSlide
+            // 
+            BotonSlide.Cursor = Cursors.Hand;
+            BotonSlide.Image = (Image)resources.GetObject("BotonSlide.Image");
+            BotonSlide.Location = new Point(6, 9);
+            BotonSlide.Name = "BotonSlide";
+            BotonSlide.Size = new Size(35, 35);
+            BotonSlide.SizeMode = PictureBoxSizeMode.Zoom;
+            BotonSlide.TabIndex = 0;
+            BotonSlide.TabStop = false;
+            BotonSlide.Click += BotonSlide_Click;
+            // 
+            // Contenedor
+            // 
+            Contenedor.Dock = DockStyle.Fill;
+            Contenedor.Location = new Point(250, 50);
+            Contenedor.Name = "Contenedor";
+            Contenedor.Size = new Size(1050, 600);
+            Contenedor.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 650);
+            Controls.Add(Contenedor);
+            Controls.Add(BarraTitulo);
+            Controls.Add(MenuVertical);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            BarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)BotonMininizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BotonCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BotonSlide).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel MenuVertical;
+        private Panel BarraTitulo;
+        private Panel Contenedor;
+        private PictureBox BotonSlide;
+        private PictureBox pictureBox1;
+        private PictureBox BotonCerrar;
+        private PictureBox BotonMininizar;
+        private Button BotonRegistrarObra;
+        private Button BotonClientes;
+        private Button BotonVentas;
+    }
+}
---
+++ BRAHO Project.cs
@@ -0,0 +1,46 @@
+using System.Runtime.InteropServices;
+
+namespace BRAHO_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //HOla
+
+        //Codigo para mover el formulario
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void BotonSlide_Click(object sender, EventArgs e)
+        {
+            if (MenuVertical.Width == 250)
+            {
+                MenuVertical.Width = 70;
+            }
+            else
+                MenuVertical.Width = 250;
+        }
+
+        private void BotonCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BotonMininizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void BarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
