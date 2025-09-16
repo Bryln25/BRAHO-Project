53a472141d36b9482ef68b59104e9665b34302f5
Andrews Hernández <andrews100404@gmail.com>
2025-09-16 16:39:06 -0400

    foro clientes

---
+++ FrmClientes.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace BRAHO_Project
+{
+    partial class FrmClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmClientes));
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            MoverVentanaInicioSesion = new Panel();
+            BotonMininizar = new PictureBox();
+            BotonCerrar = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            dgvBuscar = new DataGridView();
+            MoverVentanaInicioSesion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)BotonMininizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BotonCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvBuscar).BeginInit();
+            SuspendLayout();
+            // 
+            // MoverVentanaInicioSesion
+            // 
+            MoverVentanaInicioSesion.BackColor = Color.FromArgb(102, 102, 102);
+            MoverVentanaInicioSesion.Controls.Add(pictureBox1);
+            MoverVentanaInicioSesion.Controls.Add(pictureBox2);
+            MoverVentanaInicioSesion.Controls.Add(BotonMininizar);
+            MoverVentanaInicioSesion.Controls.Add(BotonCerrar);
+            MoverVentanaInicioSesion.Dock = DockStyle.Top;
+            MoverVentanaInicioSesion.Location = new Point(0, 0);
+            MoverVentanaInicioSesion.Name = "MoverVentanaInicioSesion";
+            MoverVentanaInicioSesion.Size = new Size(1041, 45);
+            MoverVentanaInicioSesion.TabIndex = 1;
+            // 
+            // BotonMininizar
+            // 
+            BotonMininizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BotonMininizar.Cursor = Cursors.Hand;
+            BotonMininizar.Image = (Image)resources.GetObject("BotonMininizar.Image");
+            BotonMininizar.Location = new Point(1240, 9);
+            BotonMininizar.Name = "BotonMininizar";
+            BotonMininizar.Size = new Size(29, 33);
+            BotonMininizar.SizeMode = PictureBoxSizeMode.Zoom;
+            BotonMininizar.TabIndex = 2;
+            BotonMininizar.TabStop = false;
+            // 
+            // BotonCerrar
+            // 
+            BotonCerrar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BotonCerrar.Cursor = Cursors.Hand;
+            BotonCerrar.Image = (Image)resources.GetObject("BotonCerrar.Image");
+            BotonCerrar.Location = new Point(1282, 9);
+            BotonCerrar.Name = "BotonCerrar";
+            BotonCerrar.Size = new Size(29, 33);
+            BotonCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            BotonCerrar.TabIndex = 1;
+            BotonCerrar.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(960, 7);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(29, 33);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox2.Cursor = Cursors.Hand;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1002, 7);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(29, 33);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // dgvBuscar
+            // 
+            dgvBuscar.AllowUserToAddRows = false;
+            dgvBuscar.AllowUserToDeleteRows = false;
+            dgvBuscar.AllowUserToResizeColumns = false;
+            dgvBuscar.AllowUserToResizeRows = false;
+            dgvBuscar.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvBuscar.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvBuscar.BackgroundColor = Color.FromArgb(102, 102, 102);
+            dgvBuscar.BorderStyle = BorderStyle.None;
+            dgvBuscar.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvBuscar.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.HotTrack;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvBuscar.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvBuscar.ColumnHeadersHeight = 35;
+            dgvBuscar.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 12F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvBuscar.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvBuscar.EnableHeadersVisualStyles = false;
+            dgvBuscar.Location = new Point(340, 131);
+            dgvBuscar.Margin = new Padding(4);
+            dgvBuscar.Name = "dgvBuscar";
+            dgvBuscar.ReadOnly = true;
+            dgvBuscar.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(45, 66, 91);
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = Color.SteelBlue;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dgvBuscar.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dgvBuscar.RowHeadersVisible = false;
+            dgvBuscar.RowHeadersWidth = 51;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(45, 66, 91);
+            dataGridViewCellStyle4.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = Color.SteelBlue;
+            dataGridViewCellStyle4.SelectionForeColor = Color.White;
+            dgvBuscar.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dgvBuscar.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvBuscar.Size = new Size(649, 337);
+            dgvBuscar.TabIndex = 10;
+            dgvBuscar.TabStop = false;
+            // 
+            // FrmClientes
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(127, 127, 127);
+            ClientSize = new Size(1041, 617);
+            Controls.Add(dgvBuscar);
+            Controls.Add(MoverVentanaInicioSesion);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmClientes";
+            Text = "FrmClientes";
+            MoverVentanaInicioSesion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)BotonMininizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BotonCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvBuscar).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel MoverVentanaInicioSesion;
+        private PictureBox BotonMininizar;
+        private PictureBox BotonCerrar;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private DataGridView dgvBuscar;
+    }
+}
