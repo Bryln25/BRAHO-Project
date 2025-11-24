6bb778d3377a80ff082512e51098e3c4240b2b76
Andrews Hernández <andrews100404@gmail.com>
2025-11-24 11:26:53 -0400

    nueva conexion

--- ConexionBRAHOBD.cs
+++ ConexionBRAHOBD.cs
@@ -11,8 +11,12 @@ namespace BRAHO_Project
     {
         public static SqlConnection ObtenerConexion()
         {
+            //SqlConnection conexion = new SqlConnection(
+            //    @"Data Source=25.39.110.125,1433;Initial Catalog=BrahoDB;User ID=user1;Password=12345;Encrypt=False;"
+            //);
+
             SqlConnection conexion = new SqlConnection(
-                @"Data Source=25.39.110.125,1433;Initial Catalog=BrahoDB;User ID=user1;Password=12345;Encrypt=False;"
+               @"workstation id=BrahoDB.mssql.somee.com;packet size=4096;user id=AndrewsH_SQLLogin_1;pwd=6k2wgo4tjq;data source=BrahoDB.mssql.somee.com;persist security info=False;initial catalog=BrahoDB;TrustServerCertificate=True;"
             );
 
             try
